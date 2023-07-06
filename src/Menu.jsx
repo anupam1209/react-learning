215f5f6a02242cd82178ca9e86ca95c19e4c33cd
Anupam Prakash <anupamprakash82@gmail.com>
2023-07-07 01:20:49 +0530

    feature: added some components and styling

---
+++ src/Menu.jsx
@@ -0,0 +1,11 @@
+const Menu = () => {
+  return (
+    <>
+      <main className="menu">
+        <h2>This is Menu</h2>
+        <img src="pizzas/funghi.jpg" alt="funghi" />
+      </main>
+    </>
+  );
+};
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
-}
+import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
